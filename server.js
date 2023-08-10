212c2a9868797acd228381df63f2f741bd2aa6ea
ssok305 <sutherysok@gmail.com>
2023-08-10 14:53:03 -0600

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -6,29 +6,18 @@ const router = express.Router();
 
 const PORT = 3000;
 const pokemon = require('./models/pokemon')
-const pokemonRoutes = require('./routes/pokemonRoutes')
-
-function index (req,res) {
-    res.render('./Index', {pokemon})
-} 
-
 
 const jsxEngine = require('jsx-view-engine')
 app.set('view engine', 'jsx')
 app.engine('jsx', jsxEngine())
 
-// app.use('/pokemon', pokemonRoutes)
-
-// app.get('/pokemon', (req,res) => {
-//     res.render('/Index', {})
-// })
 
 app.get('/', (req,res) =>{
     res.send('Welcome to the Pokemon App!');
 })
 
 app.get('/pokemon', (req,res) =>{
-    res.render('Index');
+    res.render('Index', {pokemon});
 })
 
 
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-function Index(){
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+function Index(props){
     return(
         <div>
             <h1>See All The Pokemon!</h1>
+            <ul>
+            {props.pokemon.map((pokemon, index) => 
+                <li key={index}>
+                    {capitalizeFirstLetter(pokemon.name)}
+                </li>
+            )}
+            </ul>
         </div>
     )
 }
